54894d1c0ae80a641e88ac9337abc87e84f2ebb6
João Aguiar <joaoaguiar.nave@gmail.com>
2023-09-21 16:20:34 -0300

    acrescimo da notinha

--- python/sanduba.py
+++ python/sanduba.py
@@ -24,5 +24,56 @@ match opcao:
         valor_total += 4
         pao_escolhido = 'Italiano'
 
-print(valor_total)
-print(pao_escolhido)
+opcao = int(input("""
+Escolha o recheio:
+    1 - Carne
+    2 - Frango
+    3 - Soja
+"""))
+
+match opcao:
+    case 1:
+        valor_total += 9
+        recheio_escolhido = 'Carne'
+    case 2:
+        valor_total += 7
+        recheio_escolhido = 'Frango'
+    case 3:
+        valor_total += 6
+        recheio_escolhido = 'Soja'
+    case other:
+        valor_total += 7
+        recheio_escolhido = 'Frango'
+
+opcao = int(input("""
+Escolha o molho:
+    1 - Agridoce
+    2 - Parmesao
+    3 - Especial
+"""))
+
+match opcao:
+    case 1:
+        valor_total += 3
+        molho_escolhido = 'Agridoce'
+    case 2:
+        valor_total += 2
+        molho_escolhido = 'Parmesao'
+    case 3:
+        valor_total += 2
+        molho_escolhido = 'Especial'
+    case other:
+        valor_total += 2
+        molho_escolhido = 'Especial'
+
+print(f"""
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    Caro cliente, conforme escolhido, seu sanduba terá:
+      Pão:                      {pao_escolhido},
+      Recheio:                  {recheio_escolhido},
+      Molho:                    {molho_escolhido}.
+
+    TOTAL DO PEDIDO:            R${valor_total:.2f}
+    Obrigado pela preferência e volte sempre!
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+""")
