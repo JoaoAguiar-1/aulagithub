d91bd8e58e0c647d1da4b73cfd2bdb80a4ab70a7
João Aguiar <joaoaguiar.nave@gmail.com>
2023-09-29 12:22:06 -0300

    escopo local/global, *args,**kargs

--- python/funcoes.py
+++ python/funcoes.py
@@ -1,4 +1,4 @@
-def ola(nome):
+'''def ola(nome):
     return f"Olá {nome}!"
 
 print(ola("joão"), 10, sep=";")
@@ -12,3 +12,38 @@ print(f'a soma será {x}')
 def dados(nome='desconhecido',sobrenome = 'desconhecido',idade ='desconhecido', genero= 'desconhecido' ):
     return f'nome: {nome} \nsobrenome {sobrenome} \nidade: {idade} \ngênero:{genero}'
 print(dados('joão'))
+
+def imc():
+    global altura,peso #usado caso queira acessar elas fora do escopo local
+    altura = float(input("Digite a altura:"))
+    peso = float(input("Digite o peso:"))
+    return peso/altura**2
+
+print(f"Seu imc é {imc():.2f}")'''
+
+#*args passamos varios valores de um mesmo tipo
+def somador(*numeros):
+    return sum(numeros)
+
+print(somador(5,1,4,6,4))
+
+def pagamentos(nome, *pagamentos):
+    print('Nome:' , nome)
+    for pagamento in pagamentos:
+        print("Valor: R$",pagamento)
+    print('Total: R$',sum(pagamentos))
+
+pagamentos('Maria', 300.00, 440.50)
+
+#**kwargs passamos valores que ocuparão a estrutura do dicionário
+def calcular(**valores):
+    return valores['valor'] + (valores['valor'] * valores['taxa'] / 100)
+print(calcular(valor = 100.00, taxa = 10))
+
+#equivalente a fazer isso:
+meu_dict = {
+    'valor' : 100.00,
+    'taxa' :10
+}
+print(meu_dict['valor'] + (meu_dict['valor'] * meu_dict['taxa']/100))
+
